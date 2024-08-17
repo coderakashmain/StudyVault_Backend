17b32bce0f69b2bfb7a7d65d38b6edc15ad3befc
Akash Bindhani <ab791235@gmail.com>
2024-08-17 15:34:33 +0530

    nothing

--- server.js
+++ server.js
@@ -614,7 +614,7 @@ app.post('/api/Profile', upload.single('file'), (req, res) => {
     })
   })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 
 app.listen(port,()=>{
