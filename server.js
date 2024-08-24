a3527ad1f89b23b34dafb6f4761e54aeb01c3352
Akash Bindhani <ab791235@gmail.com>
2024-08-24 23:49:53 +0530

    nothing

--- server.js
+++ server.js
@@ -622,6 +622,7 @@ app.post("/api/LogIn/ForgatePw/ResetPassword", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
+const ip = process.env.IP || '127.0.0.1';
 
 app.listen(port, process.env.IP, () => {
   console.log(`The website is running on port ${port}`);
