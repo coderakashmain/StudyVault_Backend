8e63f000333d0f15a440a25d13faedd91c58a5a7
Akash Bindhani <ab791235@gmail.com>
2026-04-03 11:52:59 +0530

    n

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 /////////////
 
 const port = process.env.PORT || 3000;
-const ip = process.env.IP || "127.0.0.1";
+const ip = process.env.IP || "0.0.0.0";
 
 app.listen(port, ip, () => {
   console.log(`The website is running on port ${port}`);
